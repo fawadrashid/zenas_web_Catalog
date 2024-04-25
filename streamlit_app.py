2e7d48c8eaea8562b5f4635c7d95fcb7e4675cac
Fawad Rashid <fawad.rashid@gmail.com>
2024-04-25 20:58:01 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,15 +21,16 @@ sweatshirts_selected_option = st.selectbox(
 selected_sweatshirt_product = pd_catalog.loc[pd_catalog["COLOR_OR_STYLE"] == sweatshirts_selected_option]
 #st.write(type(st.dataframe(selected_sweatshirt_product)))
 #st.write(type(selected_sweatshirt_product))
-st.write(selected_sweatshirt_product["DIRECT_URL"])
+#st.write(selected_sweatshirt_product["DIRECT_URL"])
 #st.stop()
 #st.write ('You have selected', selected_sweatshirt_product ["DIRECT_URL"])
 
 
 #st.write(selected_sweatshirt_product["DIRECT_URL"].to_string(index=False))
-sweatshirt_image_url = selected_sweatshirt_product["DIRECT_URL"]
+#sweatshirt_image_url = selected_sweatshirt_product["DIRECT_URL"]
 #st.write(type(sweatshirt_image_url))
 #st.write(sweatshirt_image_url.index[0])
+st.write(selected_sweatshirt_product)
 st.image(sweatshirt_image_url[sweatshirt_image_url.index[0]],width=400, caption="Our warn comfortable, " + selected_sweatshirt_product["COLOR_OR_STYLE"].index[0] + " sweatshirt!" )
 
 

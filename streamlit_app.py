e0a1bb17b3e190fd1a42c9d1f7da62940bb7f9a3
Fawad Rashid <fawad.rashid@gmail.com>
2024-04-25 19:51:46 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,26 +27,4 @@ st.write(selected_sweatshirt_product)
 st.write(selected_sweatshirt_product["DIRECT_URL"])
 st.image(selected_sweatshirt_product["DIRECT_URL"])
 st.stop()
-    for color_style_chooen in sweatshirts_selected_option:
-        ingredients_string += fruit_chosen + ' '
 
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-    
-    #st.write(ingredients_string)
-
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-        values ('""" + ingredients_string +  """','""" + name_on_order+ """')"""
-    #st.write(my_insert_stmt)
-    #st.stop()
-    
-    time_to_insert = st.button('Submit Order')
-    
-    
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")

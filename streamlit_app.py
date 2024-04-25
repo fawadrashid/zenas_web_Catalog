979ee53bdc694f64772a44f480999c5ee75582f0
Fawad Rashid <fawad.rashid@gmail.com>
2024-04-25 20:35:59 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,5 +29,5 @@ st.write(selected_sweatshirt_product["DIRECT_URL"])
 #st.write(selected_sweatshirt_product["DIRECT_URL"].to_string(index=False))
 sweatshirt_image_url = selected_sweatshirt_product["DIRECT_URL"]
 #st.write(type(sweatshirt_image_url))
-st.image(sweatshirt_image_url)
+st.image(sweatshirt_image_ur, output_format="PNG")
 
